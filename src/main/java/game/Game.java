6bdd01b957cfedd70cf8544f868549d2cf45099a
s6404062630082 <118630113+s6404062630082@users.noreply.github.com>
2022-11-25 00:50:39 +0700

    Update Game.java

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -22,6 +22,7 @@ import javax.swing.JTextField;
 public class Game extends JPanel implements KeyListener{
     int lv=0;
     int t=0;
+    public int enter=0;
     GunMan gunman = new GunMan(100,300,100,500); 
     Wave[] waveSet = makeWaveSet(2);
     
@@ -35,6 +36,7 @@ public class Game extends JPanel implements KeyListener{
     Image  bar2 = new ImageIcon("/Users/lookpeartayida/NetBeansProjects/Game/src/main/img/bar2.PNG").getImage();
     Image  win = new ImageIcon("/Users/lookpeartayida/NetBeansProjects/Game/src/main/img/win.PNG").getImage();
     Image  gift = new ImageIcon("/Users/lookpeartayida/NetBeansProjects/Game/src/main/img/gift.PNG").getImage();
+    Image  bgst = new ImageIcon("/Users/lookpeartayida/NetBeansProjects/Game/src/main/img/bg1.png").getImage();
     
     public JTextField jtfScreen;
     long lastPress = 0;
@@ -50,72 +52,74 @@ public class Game extends JPanel implements KeyListener{
     @Override
     public void paint(Graphics g){
         
-        
         super.paint(g);
+        
         Graphics2D g2d = (Graphics2D)g;
-        Graphics2D st =  (Graphics2D) g ;
+        Graphics2D st =  (Graphics2D) g;
+        g.drawImage(bgst,0,0,getWidth(),getHeight(),this);
         
-        g.drawImage(img1,0,0,getWidth(),getHeight(),this);
-        
-        g.drawImage(gm,gunman.x, gunman.y, gunman.gunmanSize,200,this);
-        g2d.setColor(Color.black);
-        g2d.drawString("HP ", 60, 26);
-        
-      
-        
-        
-        for(Wave wave : waveSet){
-            //Walk
+        if(this.enter==10){
             
-            if(gunman.health > 0 && lv<350){
-                g.drawImage(bar,290,100,400,100,this);
-                g.drawImage(ev,wave.x,300,gunman.gunmanSize,200,this);
-                
-                g2d.fillRect(100, 10, gunman.health, 20);
-                g2d.setColor(Color.green);
-                
-                
-            
-            }
-            //Win
-            if(gunman.health > 0 && lv==350){
+            g.drawImage(img1,0,0,getWidth(),getHeight(),this);
+            g.drawImage(gm,gunman.x, gunman.y, gunman.gunmanSize,200,this);
+            g2d.setColor(Color.black);
+            g2d.drawString("HP ", 60, 26);
+
+
+
+
+            for(Wave wave : waveSet){
+                //Walk
+
+                if(gunman.health > 0 && lv<350){
+                    g.drawImage(bar,290,100,400,100,this);
+                    g.drawImage(ev,wave.x,300,gunman.gunmanSize,200,this);
+
+                    g2d.fillRect(100, 10, gunman.health, 20);
+                    g2d.setColor(Color.green);
+
+
+
+                }
+                //Win
+                if(gunman.health > 0 && lv==350){
+                    g.drawImage(ev,0,0,0,0,this);
+                    g.drawImage(win,300,200,500,200,this);
+                    g.drawImage(gift,400,250,200,200,this);
+
+                }
+
+                //GameOver
+                if(gunman.health <= 0){
                 g.drawImage(ev,0,0,0,0,this);
-                g.drawImage(win,300,200,500,200,this);
-                g.drawImage(gift,400,250,200,200,this);
-                
-            }
-            
-            //GameOver
-            if(gunman.health <= 0){
-            g.drawImage(ev,0,0,0,0,this);
-            g.drawImage(over,300,200,500,200,this);
-            
+                g.drawImage(over,300,200,500,200,this);
+
+
+                }
+                if(Event.checkHit(gunman, wave)){//static method
+                  gunman.health -=0.05;
+                  this.repaint();
+                  g2d.setColor(Color.red);
+                  g2d.fillRect(100, 10,gunman.health, 20);
+                  g2d.setColor(Color.red);
+
+
+                }
+
+
 
             }
-            if(Event.checkHit(gunman, wave)){//static method
-              gunman.health -=0.05;
-              this.repaint();
-              g2d.setColor(Color.red);
-              g2d.fillRect(100, 10,gunman.health, 20);
-              g2d.setColor(Color.red);
-               
-            
+
+            g.drawImage(bar2,290+lv,100,60,60,this);  
+            if(gunman.health > 0 && lv<350 && t%50==0){
+                    lv+=10; 
+                    this.repaint();
+                    g.drawImage(bar2,290+lv,100,60,60,this); 
             }
-            
-           
-        
+              t++;
+
         }
-        
-        g.drawImage(bar2,290+lv,100,60,60,this);  
-        if(gunman.health > 0 && lv<350 && t%50==0){
-                lv+=10; 
-                this.repaint();
-                g.drawImage(bar2,290+lv,100,60,60,this); 
-        }
-          t++;
-        
     }
-   
     
     private Wave[] makeWaveSet(int waveNumber){ //สร้างเวฟ
         if(gunman.health <= 0){
@@ -143,18 +147,17 @@ public class Game extends JPanel implements KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==10){
+             System.out.println(e.getKeyCode());
+                this.enter=10;          
+        }
         if(System.currentTimeMillis()-lastPress>400){
                 System.out.println(e.getKeyCode());
             if(e.getKeyCode()==38||e.getKeyCode()==32){
                 gunman.jump(this);
                 this.repaint();
             
-        } lastPress= System.currentTimeMillis();
-        if(e.getKeyCode()==38||e.getKeyCode()==32 || gunman.health<=0){
-                Game g = new Game();
-
-            
-        } 
+            } lastPress= System.currentTimeMillis();
         }
         
     }
